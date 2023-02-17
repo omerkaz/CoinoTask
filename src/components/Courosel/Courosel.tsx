c6c2610000da7b6c15fe39ff1428d700945c566e
Ömer KAZ <trmn500@gmail.com>
2023-02-17 23:33:55 +0300

    chore: update existing Carousel component in src/components/Courosel chore: update existing Footer component in src/components/Layout feat: create Footer.module.scss to src/components/Layout

--- src/components/Courosel/Courosel.tsx
+++ src/components/Courosel/Courosel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Carousel.module.scss";
 
 type CarouselProps = {
@@ -7,7 +7,25 @@ type CarouselProps = {
 };
 
 function Caurosel({ images, text }: CarouselProps) {
-  return <div className={styles.carousel}>Caurosel</div>;
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  };
+  return (
+    <section className={styles.carousel}>
+      <div>İmage</div>
+      <div>Text</div>
+      <div>Navigation
+        <button></button>
+        <button></button>
+      </div>
+    </section>
+  );
 }
 
 export default Caurosel;
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import styles from "./Footer.module.scss";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <div className="">
+            <div>Information</div>
+            <div>Subscribe</div>
+          </div>
+        </div>
+      </footer>
+      <div className={styles.copyright}>© 2023 All Rights Reserved.</div>
+    </>
+  );
 }
 
 export default Footer;
