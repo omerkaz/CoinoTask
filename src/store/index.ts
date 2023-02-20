05703561dc9faeff20821ef6d2c27e0304a13588
Ömer KAZ <trmn500@gmail.com>
2023-02-20 18:32:20 +0300

    feat: implemented redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import cartReducers from "./cart/slice";
 import favoritesReducers from "./favorites/slice";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedCartReducer = persistReducer(persistConfig, cartReducers);
+const persistedFavoritesReducer = persistReducer(
+  persistConfig,
+  favoritesReducers
+);
+
 const store = configureStore({
   reducer: {
-    cart: cartReducers,
-    favorites: favoritesReducers,
+    cart: persistedCartReducer,
+    favorites: persistedFavoritesReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
